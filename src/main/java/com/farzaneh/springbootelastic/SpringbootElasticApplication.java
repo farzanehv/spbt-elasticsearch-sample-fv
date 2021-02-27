c08267361115023729b1e70891d11d9a09d85a02
farzaneh <farzaneh.v@gmail.com>
2021-02-27 21:32:36 +0330

    spring boot structure

---
+++ src/main/java/com/farzaneh/springbootelastic/SpringbootElasticApplication.java
@@ -0,0 +1,13 @@
+package com.farzaneh.springbootelastic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootElasticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootElasticApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/farzaneh/springbootelastic/SpringbootElasticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farzaneh.springbootelastic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootElasticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
