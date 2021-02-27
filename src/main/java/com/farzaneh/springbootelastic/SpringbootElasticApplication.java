9eb18c29ea5fef839eda219c2dfbcebfe03674ba
farzaneh <farzaneh.v@gmail.com>
2021-02-27 22:40:33 +0330

    spring model files

--- src/main/java/com/farzaneh/springbootelastic/SpringbootElasticApplication.java
+++ src/main/java/com/farzaneh/springbootelastic/SpringbootElasticApplication.java
@@ -1,13 +1,60 @@
 package com.farzaneh.springbootelastic;
 
+import com.farzaneh.springbootelastic.book.model.Book;
+import com.farzaneh.springbootelastic.book.service.BookService;
+import org.elasticsearch.client.Client;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import java.util.Map;
 
 @SpringBootApplication
-public class SpringbootElasticApplication {
+public class SpringbootElasticApplication implements CommandLineRunner{
+
+	
+	@Autowired
+   	private ElasticsearchOperations es;
+
+    	@Autowired
+    	private BookService bookService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootElasticApplication.class, args);
 	}
 
+	@Override
+    	public void run(String... args) throws Exception {
+
+        	printElasticSearchInfo();
+
+       		 bookService.save(new Book("1001", "Elasticsearch Basics", "Rambabu Posa", "23-FEB-2017"));
+       		 bookService.save(new Book("1002", "Apache Lucene Basics", "Rambabu Posa", "13-MAR-2017"));
+       		 bookService.save(new Book("1003", "Apache Solr Basics", "Rambabu Posa", "21-MAR-2017"));
+
+        //fuzzey search
+        Page<Book> books = bookService.findByAuthor("Rambabu", new PageRequest(0, 10));
+
+        //List<Book> books = bookService.findByTitle("Elasticsearch Basics");
+
+        books.forEach(x -> System.out.println(x));
+
+
+    }
+
+    //useful for debug, print elastic search details
+    private void printElasticSearchInfo() {
+
+        System.out.println("--ElasticSearch--");
+        Client client = es.getClient();
+        Map<String, String> asMap = client.settings().getAsMap();
+
+        asMap.forEach((k, v) -> {
+            System.out.println(k + " = " + v);
+        });
+        System.out.println("--ElasticSearch--");
+    }
 }
--- src/test/java/com/farzaneh/springbootelastic/SpringbootElasticApplicationTests.java
+++ src/test/java/com/farzaneh/springbootelastic/SpringbootElasticApplicationTests.java
@@ -1,13 +1,13 @@
-package com.farzaneh.springbootelastic;
+//package com.farzaneh.springbootelastic;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class SpringbootElasticApplicationTests {
+//@SpringBootTest
+//class SpringbootElasticApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
-}
+//}
